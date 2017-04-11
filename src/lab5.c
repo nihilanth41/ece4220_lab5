47967506d5b7c823d00b86bd4503454e810f7ad7
Zachary Rump <zrrm74@mail.missouri.edu>
2017-04-10 22:07:04 -0500

    Tally vote

--- src/lab5.c
+++ src/lab5.c
@@ -14,13 +14,16 @@
 #define MSG_SIZE 40
 #define IP_MAX 10
 #define IP_MIN 1
-#define VOTE_READ_TIMEOUT_USEC 500000
+#define VOTE_TIMEOUT_SEC 5
+#define BROADCAST_ADDR 10.3.52.255
 
 int main(int argc, char **argv) {
   int sock, n;
   struct sockaddr_in server;
   struct sockaddr_in from;
   struct hostent *hp = NULL;
+  unsigned long broadcast_addr = inet_addr("BROADCAST_ADDR");
+  unsigned long server_addr; 
   int length = sizeof(server);
   socklen_t fromlen = sizeof(struct sockaddr_in);
   unsigned int opt_val_bool = 1; // To pass to setsockopt()
@@ -29,7 +32,7 @@ int main(int argc, char **argv) {
   char str_buf[MSG_SIZE]; // for sprintf
   char tok_buf[MSG_SIZE]; // for strtok
   char ip_str[INET_ADDRSTRLEN];
-  struct timeval read_timeout = { .tv_sec = 0, .tv_usec = VOTE_READ_TIMEOUT_USEC };
+  struct timeval read_timeout = { .tv_sec = VOTE_TIMEOUT_SEC, .tv_usec = 0};
   int VOTES[IP_MAX + 1] = { 0 };
 
   // Check input args for port #
@@ -100,12 +103,16 @@ int main(int argc, char **argv) {
 	   printf("WHOIS ack.\n");
 	   if(isMaster)
 	     {
+	       server_addr = server.sin_addr.s_addr;
+	       server.sin_addr.s_addr = broadcast_addr;
 	       sprintf(&str_buf, "Zach on board %s is the master", ip_str);
 	       // Write message to broadcast address.
+	       // TODO change to broadcast
 	       n = sendto(sock, &str_buf, strlen(str_buf), 0, (struct sockaddr *)&server, fromlen);
 	       if(n < 0) {
 		 perror("sendto");
 	       }
+	       server.sin_addr.s_addr = server_addr;
 	     }
 	   else // Wait for another master to reply or a VOTE
 	     {
@@ -116,21 +123,60 @@ int main(int argc, char **argv) {
 	       printf("Received datagram. It says: %s %d", buf, n);
 	       if(0 == strcmp(buf, "VOTE\n"))
 		 {
+		   // Clear votes
+		   memset(VOTES, 0, (IP_MAX + 1)*sizeof(int));
+		   bzero(tok_buf, MSG_SIZE);
 		   printf("Voting initiated.\n");
 		   // No master reply =>
 		   // Send and receive votes
-
 		   // Get random number between 1-10 for the last octet
 		   // Zeroes represent the min number
 		   int r = rand() % (IP_MAX + 1 - IP_MIN) + IP_MIN;
+		   VOTES[r]++;
 		   sprintf(str_buf, "# 10.3.52.%d", r);
-		   // Send vote
+		   // Send vote to broadcast
+		   server.sin_addr.s_addr = broadcast_addr;
 		   n = sendto(sock, &str_buf, MSG_SIZE, 0, (struct sockaddr *)&server, fromlen);
 		   if(n < 0) {
 		     perror("sendto");
 		   }
-		   // Set timeout
-		 }
+		   
+		   // Set timeout and read votes
+		   read_timeout.tv_sec = VOTE_TIMEOUT_SEC;
+		   if(setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof(read_timeout)) < 0) {
+		     perror("Error setting socket timeout");
+		   }
+		   do {
+		     n = recvfrom(sock, buf, MSG_SIZE, 0, (struct sockaddr *)&from, &fromlen);
+		     // Check buf for leading '#' and parse ip
+		     if('#' == buf[0]) {
+		       printf("Vote received\n%s", buf);
+		       strncpy(&tok_buf, &buf, strlen(buf));
+		       char *vote_num = strtok(&tok_buf, ".");
+		       int i;
+		       for(i=0; i<3; i++)
+			 {
+			   vote_num = strtok(NULL, ".");
+			 }
+		       printf("Vote: %s\n", vote_num);
+		     }
+		     bzero(buf, MSG_SIZE);
+		     bzero(tok_buf, MSG_SIZE);
+
+		     // Add vote
+		   } while(n > 0);
+
+		   // Disable timeout
+		   read_timeout.tv_sec = 0;
+		    if(setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof(struct timeval)) < 0) {
+		     perror("Error un-setting socket timeout");
+		   }
+		    // Change address back from broadcast
+		   server.sin_addr.s_addr = server_addr;
+
+		   // Tally votes
+
+		 } // Voting not initiated
 		else
 		  {
 		    // Master replied
